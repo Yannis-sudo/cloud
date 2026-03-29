424681b92286f9be1e340db300cf2d0d270a3f8e
Yannis-sudo <ya10be@gmx.de>
2026-03-29 17:26:04 +0200

    added cors

--- backend/app/config.py
+++ backend/app/config.py
@@ -10,7 +10,8 @@ class Settings:
     DATABASE_URL: str = os.getenv("DATABASE_URL", "mongodb://localhost:27017")
     DATABASE_NAME: str = os.getenv("DATABASE_NAME", "cloud")
     CORS_ORIGINS: list[str] = [
-        "http://localhost:5173",  # React dev server
+        "http://localhost:5173",  # React dev 
+        "http://10.168.6.136:5173",
     ]
 
 
